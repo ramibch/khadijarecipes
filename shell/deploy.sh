c119c7d7e144d0695569e3fc37772144afa50239
ramibch <ramiboutas@hotmail.com>
2026-02-18 16:24:06 +0100

    renaming services

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,5 +1,5 @@
 git pull
 uv run manage.py migrate
 uv run manage.py compilemessages
-sudo systemctl restart huey_kh.service
-sudo systemctl restart gunicorn_kh.service
+sudo systemctl restart huey_khadijarecipes.service
+sudo systemctl restart gunicorn_khadijarecipes.service
